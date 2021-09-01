45ea777b360f0ca0c5944ce30f90ac57a578f5d1
Alejo L <alejocartolano54@gmail.com>
2021-09-01 18:31:44 -0300

    Mas tarea de la practica 8

---
+++ Practica 8/5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+void main(void){
+	
+	char palabra[20]={"axca"};
+	char palabra2[20];
+	strcpy(palabra2,palabra);
+	strrev(palabra2);
+	printf("Palabra: %s\n",palabra);
+	printf("Reverso: %s\n",palabra2);
+	
+	if(strcmp(palabra,palabra2)==0) printf("La palabra es un palindromo!");
+	else printf("La palabra no es un palindromo");
+	
+}
---
+++ Practica 8/7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+
+int main(){
+	
+	char nombre[20];
+	printf("Ingrese su nombre en mayusculas");
+	gets(nombre);
+	if(nombre[0]=='A') strlwr(nombre);
+	printf("%s",nombre);
+	
+	getch();
+	return 0;
+}
